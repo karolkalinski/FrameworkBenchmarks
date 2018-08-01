14718a705045b6faab545a32decc54517d936a83
Karol Kaliński <karol.kalinski@hltech.com>
2018-08-01 17:29:20 +0200

    user creation modified to create-oracle.sql script

--- toolset/databases/oracle/create-oracle-database.sql
+++ toolset/databases/oracle/create-oracle-database.sql
@@ -1,3 +1,5 @@
-CREATE USER benchmarkdbuser IDENTIFIED BY benchmarkdbuser ACCOUNT UNLOCK;
+CREATE USER benchmarkdbuser IDENTIFIED BY benchmarkdbuser;
 
 GRANT CREATE SESSION TO benchmarkdbuser WITH ADMIN OPTION;
+
+ALTER USER benchmarkdbuser IDENTIFIED BY benchmarkdbuser ACCOUNT UNLOCK;
